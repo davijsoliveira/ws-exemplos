1c510920a9ee781febd4494a32c991fbf69c62f7
davijsoliveira <davijunio@gmail.com>
2023-10-16 19:34:20 -0300

    Retirada de comentários

--- parking-system/payment-terminal/payment-terminal.py
+++ parking-system/payment-terminal/payment-terminal.py
@@ -3,13 +3,12 @@ import requests
 # URL base do web service
 url = 'http://localhost:5000/payed' 
 
-# Dados para as operações matemáticas
+# Informações do ticket
 ticket01 = {
     'numero': 4,
     'valor': 15,
     'tempo': '5h'
 }
-#ticket01 = {'numero': 1, 'valor': 19, 'tempo': 5}
 
 # Cadastrando ticket
 response = requests.post(url, json=ticket01)
@@ -18,8 +17,5 @@ if response.status_code == 200:
     print("Ticket adicionado com sucesso!")
 else:
     print(f"Erro: {response.status_code} - {response.json().get('erro')}")
-
-#response = requests.post(f'{url}/payed', json=ticket01)
-#print(f'Resultado: {response.json()["mensagem"]}')
 
 
